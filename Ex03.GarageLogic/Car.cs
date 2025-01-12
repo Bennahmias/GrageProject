abce8c43daa37f4879c1d135b4958e30ba3e038f
Ben Nahmias <benna2@mta.ac.il>
2025-01-12 13:50:45 +0200

    more little constructors and changes

--- Ex03.GarageLogic/Car.cs
+++ Ex03.GarageLogic/Car.cs
@@ -1,20 +1,27 @@
 ﻿using System;
+using System.Dynamic;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
 namespace Ex03.GarageLogic
 {
     public class Car : Vehicle
     {
-        private Color.e_Color m_Color;
-        private DoorsNumber.e_DoorsNumber m_DoorsNumber;
-        private EnergyType m_EnergyType;
-        public Car(Color.e_Color i_Color, DoorsNumber.e_DoorsNumber i_DoorsNumber, Gasoline i_GasoLine, String i_ModelName,
-            String i_LicenseNumber, float i_LeftEnergy, float i_CurrentAirPressure, float i_MaxAirPressure)
-            : base(i_ModelName, i_LicenseNumber, (i_GasoLine.CurrentFuelLeft/i_GasoLine.MaxFuelTank),i_CurrentAirPressure,
-                i_MaxAirPressure,5,"MICHLEN")
+        private Color.eColor m_Color;
+        private DoorsNumber.eDoorsNumber m_DoorsNumber;
+        public const int k_NumberOfWheels = 5;
+        private const float k_MaxAirPressureCar = 34.0f;
+        public Car(string i_LicenseNumber, EnergyType i_EnergyType) : base(i_LicenseNumber, i_EnergyType)
         {
-            // Exeptions for gasoline
-            m_Color = i_Color;
-            m_DoorsNumber = i_DoorsNumber;
+            for (int i = 0; i < k_NumberOfWheels; i++)
+            {
+                m_VehicleWheels.Add(new Wheel(k_MaxAirPressureCar));
+            }
         }
+
+        public Color.eColor Color {get; set;}
+        public DoorsNumber.eDoorsNumber DoorsNumber {get; set;}
+        
+
+
     }
 }
--- Ex03.GarageLogic/Progrram.cs
+++ Ex03.GarageLogic/Progrram.cs
@@ -6,9 +6,9 @@ namespace Ex03.GarageLogic
     {
         public static void Main()
         {
-            Gasoline gasoline = new Gasoline(Gasoline.e_GasType.Octan95, 15.0f, 30.0f); // Problema 1
-            Car c1 = new Car(Color.e_Color.black, DoorsNumber.e_DoorsNumber.Four, gasoline, "BYD", "335Bd5E", 50.0f,34.0f,
-                34.0f); // Problema 2
+            //Gasoline gasoline = new Gasoline(Gasoline.eGasType.Octan95, 15.0f, 30.0f); // Problema 1
+            //Car c1 = new Car(Color.eColor.black, DoorsNumber.eDoorsNumber.Four, gasoline, "BYD", "335Bd5E", 50.0f,34.0f,
+              //  34.0f); // Problema 2
         }
     }
 }
