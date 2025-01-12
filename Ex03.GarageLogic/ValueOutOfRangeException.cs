60489d0f3a76f493f0e27915b4d3a7b0d8db37bf
Ben Nahmias <benna2@mta.ac.il>
2025-01-12 20:24:25 +0200

    delete Program for logic and add Program to UI

--- Ex03.GarageLogic/ValueOutOfRangeException.cs
+++ Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ex03.GarageLogic
 {
-    internal class ValueOutOfRangeException
+    public class ValueOutOfRangeException : Exception
     {
+        private float m_MinValue;
+        private float m_MaxValue;
+
+        public ValueOutOfRangeException(float i_MinValue, float i_MaxValue) 
+            : base(string.Format("Value out of range! Must be between {0} and {1}",i_MinValue,i_MaxValue))
+        {
+            m_MaxValue = i_MaxValue;
+            m_MinValue = i_MinValue;
+        }
     }
 }
