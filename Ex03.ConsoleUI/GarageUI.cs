b0c9780173b8e8b6393836da0d58ac564affb9ab
Ben Nahmias <benna2@mta.ac.il>
2025-01-12 20:44:03 +0200

    add UI classes

---
+++ Ex03.ConsoleUI/GarageUI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Ex03.GarageLogic;
+
+namespace Ex03.ConsoleUI
+{
+    public class GarageUI
+    {
+        private Garage m_Garage;
+        private bool m_ExitFromSystem;
+
+        public GarageUI()
+        {
+            m_Garage = new Garage();
+            m_ExitFromSystem = false;
+        }
+
+
+
+
+
+
+
+
+    }
+}
